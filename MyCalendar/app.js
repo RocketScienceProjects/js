e19d7c0c364094d7f7169ebca014dfa763ef09c8
okram999 <nirish.okram@gmail.com>
2017-08-31 23:39:56 -0500

    Completed the app v1.0

--- MyCalendar/app.js
+++ MyCalendar/app.js
@@ -1,1 +1,16 @@
-alert("Connected!!!")
+
+  //prompt for the action & store the input
+  var input = prompt("What would you like to do? Options: new/list/quit")
+  //define an empty array to hold the list
+  var todo = [];
+  //body of the app
+  while(input !== "quit"){
+  if(input === "new"){
+    var item = prompt("Enter the item for todo")
+    todo.push(item);
+  } else if(input === "list") {
+    console.log(todo);
+  }
+  var input = prompt("What would you like to do? Options: new/list/quit");
+  }
+  console.log("Goodbye & Have a nice day!!!!")
