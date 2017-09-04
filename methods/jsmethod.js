e50477d0863193ec05d590400de1b1b15c31bc53
okram999 <nirish.okram@gmail.com>
2017-09-04 12:32:26 -0500

    Adding methods

---
+++ methods/jsmethod.js
@@ -0,0 +1,22 @@
+/*
+when a function is out of the properties/attribute of an object, it is known also
+called a method in javascript
+Fucntion is used in objects:
+1. Avoid Namespace collision
+ */
+
+var workout = {
+  set1: "benchpress",
+  set2: "incline press",
+  set3: "fly",
+  //notice the function name, its still a symbol and doesn't need the var keyword
+  calories: function(x,y) {
+    return x + y;
+  }
+}
+
+workout.energyLevel = "Strong"
+
+console.log(workout)
+
+console.log(workout.calories(3,4))
